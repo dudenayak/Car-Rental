f4142bd9fce6c63f53d5c03c930dc7edcb70f20a
dudenayak <deveshjain2k1@gmail.com>
2023-01-27 13:01:08 +0530

    changes

--- cars.js
+++ cars.js
@@ -1,4 +1,5 @@
 const carForm = document.querySelector('.carForm');
+const car_container = document.querySelector('.car_container');
 
 const carsData = function () {
   var idb = indexedDB.open('Cars', 1);
@@ -18,7 +19,7 @@ const carsData = function () {
     var tx = request.transaction('CarsData', 'readwrite');
     var store = tx.objectStore('CarsData');
     if (checkEmpty()) {
-      alert('User registered successfully! Login NOW!');
+      alert('Car added successfully!');
       store.put({
         carName: carForm[0].value,
         carYear: carForm[1].value,
@@ -27,7 +28,7 @@ const carsData = function () {
         carAvg: carForm[4].value,
         carMode: carForm[5].value,
         carPrice: carForm[6].value,
-        // confirmPassword: form[3].value
+        carImage: carForm[7].value,
       });
       location.reload();
     } else {
@@ -45,6 +46,89 @@ function checkEmpty() {
     carForm[3].value != '' &&
     carForm[4].value != '' &&
     carForm[5].value != '' &&
-    carForm[6].value != ''
+    carForm[6].value != '' &&
+    carForm[7].value != ''
   );
 }
+
+function read() {
+  var idb = indexedDB.open('Cars', 1);
+  idb.onsuccess = function () {
+    var res = idb.result;
+    var tx = res.transaction('CarsData', 'readonly');
+    var store = tx.objectStore('CarsData');
+    var cursor = store.openCursor();
+    cursor.onsuccess = function () {
+      var curRes = cursor.result;
+      if (curRes) {
+        // console.log(curRes);
+        car_container.innerHTML += `
+        <ul class="featured-car-list">
+        <li>
+            <div class="featured-car-card" >
+            <figure class="card-banner">
+                        <img src="./assets/images/car-1.jpg" alt="Toyota RAV4 2021" loading="lazy" width="440"
+                            height="300" class="w-100">
+                            <a href="#">${curRes.value.carImage}</a>
+                    </figure>
+                <div class="card-content">
+                    <div class="card-title-wrapper">
+                        <h3 class="h3 card-title">
+                            <a href="#">${curRes.value.carName}</a>
+                        </h3>
+                        <data class="year" value="2021">${curRes.value.carYear}</data>
+                    </div>
+                    
+                    <ul class="card-list">
+
+                        <li class="card-list-item">
+                            <ion-icon name="people-outline"></ion-icon>
+                            <span class="card-item-text">${curRes.value.carSpace} People</span>
+                        </li>
+
+                        <li class="card-list-item">
+                            <ion-icon name="flash-outline"></ion-icon>
+
+                            <span class="card-item-text">${curRes.value.carType}</span>
+                        </li>
+
+                        <li class="card-list-item">
+                            <ion-icon name="speedometer-outline"></ion-icon>
+
+                            <span class="card-item-text">${curRes.value.carAvg}km / 1-litre</span>
+                        </li>
+
+                        <li class="card-list-item">
+                            <ion-icon name="hardware-chip-outline"></ion-icon>
+
+                            <span class="card-item-text">${curRes.value.carMode}</span>
+                        </li>
+
+                    </ul>
+
+                    <div class="card-price-wrapper">
+
+                        <p class="card-price">
+                            <strong>₹${curRes.value.carPrice}</strong> / hour
+                        </p>
+
+                        <button class="btn fav-btn" aria-label="Add to favourite list">
+                            <ion-icon name="heart-outline"></ion-icon>
+                        </button>
+
+                        <button class="btn">Rent now</button>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </li>
+    </ul>
+        `;
+        curRes.continue();
+      }
+    };
+  };
+}
+read();
